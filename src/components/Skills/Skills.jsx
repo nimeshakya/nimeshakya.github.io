619d840df426279b977517ab2fdc91b0d1d55bb7
nimeshakya <nimeshakyabtw@gmail.com>
2023-09-15 12:28:33 +0545

    skills section complete

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
+import { RxTriangleRight } from 'react-icons/rx';
 
 import UnderLine from '../UnderLine/UnderLine';
 
 import './Skills.scss';
 
 const Skills = () => {
+    const skills = [
+        'HTML5',
+        'CSS3',
+        'TailwindCSS',
+        'SASS',
+        'JavaScript (ES6+)',
+        'ReactJS',
+    ];
+
     return (
         <article id='skills'>
             <h1 className='article-heading'>Skills</h1>
             <UnderLine />
             <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
-                quia quod cum tempora error. Laborum, officiis accusantium
-                laudantium ratione debitis asperiores deleniti animi nostrum non
-                tempora, consequuntur vitae qui voluptatibus ducimus omnis harum
-                repellendus suscipit repudiandae. Nostrum, saepe! Atque,
-                molestiae.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+                recusandae animi tempora eaque totam autem, dolores repellendus
+                architecto minus perferendis quae atque iure dolore molestiae?
             </p>
+            <ul>
+                {skills.map((skill, index) => {
+                    return (
+                        <li>
+                            <span>
+                                <RxTriangleRight />
+                            </span>
+                            <span>{skill}</span>
+                        </li>
+                    );
+                })}
+            </ul>
         </article>
     );
 };
