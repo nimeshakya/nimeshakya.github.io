2530cf3d1adb97f84f1c2a415b711873b4f6df6d
nimeshakya <nimeshakyabtw@gmail.com>
2024-06-13 11:23:20 +0545

    Changed base to current domain (www.nimeshakya.com.np).

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/nimeshakya.github.io/',
+    base: '/www.nimeshakya.com.np/',
     plugins: [react()],
 });
